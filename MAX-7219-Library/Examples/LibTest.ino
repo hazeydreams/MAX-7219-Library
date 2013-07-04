6d2c792b0076fb874f76d99817d1dd6a72337ee8
someguyeatingahamburger <dalinriches@gmail.com>
2013-07-03 23:59:53 -0600

    Added an example sketch
    This sketch runs through each function in order to show what they look
    like.

---
+++ MAX-7219-Library/Examples/LibTest.ino
@@ -0,0 +1,25 @@
+#include <MAX.h>
+
+MAX run;
+
+void setup(){}
+void loop(){
+  
+run.test(500);
+run.a(500);
+run.b(500);
+run.c(500);
+run.d(500);
+run.e(500);
+run.f(500);
+run.g(500);
+run.scrllA(1,100);
+delay(500);
+run.scrllB(1,100);
+delay(500);
+run.scrllC(1,100);
+delay(500);
+run.scrllD(1,100);
+delay(1000);
+
+}
